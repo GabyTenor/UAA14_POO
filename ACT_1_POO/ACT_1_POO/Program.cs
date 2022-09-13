2c93833e86063dbc044c8195a85b266c2c1f6141
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-09-13 15:16:11 +0200

    Exercice 1

---
+++ ACT_1_POO/ACT_1_POO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ACT_1_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int average;
+
+            string tp = "";
+
+            chien[] Hund = new chien[3];
+
+            for (int i = 0; i < Hund.Length; i++)
+            {
+                Console.WriteLine("Entrez le nom du chien");
+                string nomChien = Console.ReadLine();
+
+                uint ageChien;
+                do
+                {                    
+                    Console.WriteLine("Entrez l'age du chien");
+                    tp = Console.ReadLine();
+                }while (!uint.TryParse(tp, out ageChien));
+                
+                Console.WriteLine("Entrez la race du chien");
+                string raceChien = Console.ReadLine();
+
+                Hund[i] = new chien(nomChien, ageChien, raceChien);
+                Hund[i].AfficheCaracteristiques();
+            }
+
+            
+        }
+    }
+}
---
+++ ACT_1_POO/ACT_1_POO/chien.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACT_1_POO
+{
+    class chien
+    {
+
+        private string _nom;
+        private uint _age;
+        private string _race;
+
+        public chien(string nom, uint age, string race)
+        {
+                this._nom = nom;
+                this._age = age;
+                this._race = race;
+        }
+
+        public void AfficheCaracteristiques()
+        {
+                Console.WriteLine("Nom : " + this._nom + " - Age : " + this._age + " - Race : " + this._race);         
+        }
+
+        static void Moyenne(out int average, chien[] Hund)
+        {
+            average = 0;
+            int somTotale;
+
+            for (int i = 0; i < Hund.Length; i++)
+            {
+                somTotale = somTotale + Hund[i]._age;
+            }
+        }
+    }
+}
