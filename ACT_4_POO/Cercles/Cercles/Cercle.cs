7d367106fd043d7f9158dbaf13359a0251042f75
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-11-15 14:44:33 +0100

    --

---
+++ ACT_4_POO/Cercles/Cercles/Cercle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cercles
+{
+    public class Cercle
+    {
+        private double _rayon;
+
+        public Cercle(double rayon)
+        {
+            this._rayon = rayon;
+        }
+
+        public double Rayon
+        {
+            get { return _rayon; }
+            set { _rayon = value; }
+        }
+
+        public double CalculeAire()
+        {
+            double aire = Math.PI * Math.Pow(_rayon, 2);
+
+            return aire;
+        }
+
+        public double CalculePerimetre()
+        {
+            double perimetre = 2 * Math.PI * _rayon;
+
+            return perimetre;
+        }
+
+        public void Caracteristiques(double perimetre, double aire)
+        {
+            Console.WriteLine("Le cercle de rayon " + _rayon + " a un périmètre de " + perimetre + " et une aire de " + aire);
+        }
+    }
+}
---
+++ ACT_4_POO/Cercles/Cercles/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cercles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cercle> cercles = new List<Cercle>();
+            
+            string entree;
+            double rayon;
+
+            string utiliser = " ";
+
+            int numeroCercle = -1;
+
+            Console.WriteLine("Bienvenue dans ce programme sur le cercle !");
+
+            do
+            {
+                numeroCercle++;
+                Console.WriteLine("Tapez un rayon pour votre cercle.");
+                do
+                {
+                    entree = Console.ReadLine();
+                } while (!double.TryParse(entree, out rayon));
+
+                cercles.Add(new Cercle(rayon));
+
+                double aire = cercles[numeroCercle].CalculeAire();
+                double perimetre = cercles[numeroCercle].CalculePerimetre();
+
+                cercles[numeroCercle].Caracteristiques(perimetre, aire);
+
+                /*Console.WriteLine("Voulez-vous voir tous vos cercles?");
+                string voirTout = Console.ReadLine();
+                if(voirTout == "oui")
+                {
+                    foreach(Cercle i in cercles)
+                    {
+                        cercles[i].Caracteristiques(perimetre, aire);
+                    }
+                }*/
+
+                Console.WriteLine("Un autre cercle? (Tapez sur espace)");
+                utiliser = Console.ReadLine();
+
+            } while (utiliser == " ");
+        }
+    }
+}
