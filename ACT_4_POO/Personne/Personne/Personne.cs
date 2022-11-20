cd69ae73347651ccf70b4fcd73f54fda4be71556
GabyTenor <200198@site.asty-moulin.be>
2022-11-20 10:50:58 +0100

    Personnes Transactions

---
+++ ACT_4_POO/Personne/Personne/Personne.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personne
+{
+    internal class Personne
+    {
+        private string _name;
+        private double _porteMonnaie;
+
+        public Personne(string name, double porteMonnaie)
+        {
+            this._name = name;
+            this._porteMonnaie = porteMonnaie;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public double PorteMonnaie
+        {
+            get { return _porteMonnaie; }
+            set { _porteMonnaie = value; }
+        }
+
+        public bool SiAssezDargent(double donnerArgent)
+        {
+            bool assezDargent;
+            
+            if(donnerArgent <= _porteMonnaie)
+            {
+                assezDargent = true;
+            }
+            else
+            {
+                assezDargent = false;
+            }
+
+            return assezDargent;
+        }
+
+        public void RetirerArgent(double donnerArgent)
+        {
+            _porteMonnaie = _porteMonnaie - donnerArgent;
+        }
+
+        public void RecevoirArgent(double donnerArgent)
+        {
+            _porteMonnaie = _porteMonnaie + donnerArgent;
+        }
+
+    }
+}
---
+++ ACT_4_POO/Personne/Personne/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Personne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom;
+
+            double argent;
+            
+            double donnerArgent;
+
+            string recommencer = "";
+
+            Personne[] personnes = new Personne[2];
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenue dans notre gestionnaire de monnaie !");
+
+                for (int i = 1; i < 3; i++)
+                {
+                    Console.WriteLine("Personne " + i + ", quel est votre nom?");
+                    nom = Console.ReadLine();
+                    string argentTemp;
+                    Console.WriteLine("Tapez une somme d'argent en euros");
+                    do
+                    {
+                        argentTemp = Console.ReadLine();
+                    } while (!double.TryParse(argentTemp, out argent));
+                    personnes[i - 1] = new Personne(nom, argent);
+                }
+
+                string donnerArgentTemp;
+                for (int j = 0; j < personnes.Length; j++)
+                {
+                    switch (j)
+                    {
+                        case 0:
+
+                            Console.WriteLine(personnes[j].Name + " combien voulez-vous donner à " + personnes[j + 1].Name);
+                            do
+                            {
+                                donnerArgentTemp = Console.ReadLine();
+                            } while (!double.TryParse(donnerArgentTemp, out donnerArgent));
+
+                            if (personnes[j].SiAssezDargent(donnerArgent))
+                            {
+                                personnes[j].RetirerArgent(donnerArgent);
+                                personnes[j + 1].RecevoirArgent(donnerArgent);
+                                Console.WriteLine("Ajout effectué!");
+                            }
+                            else
+                            {
+                                Console.WriteLine(personnes[j].Name + " vous n'avez pas assez d'Argent pour cette transaction");
+                            }
+
+                            break;
+
+                        case 1:
+                            Console.WriteLine(personnes[j].Name + " combien voulez-vous donner à " + personnes[j - 1].Name);
+                            do
+                            {
+                                donnerArgentTemp = Console.ReadLine();
+                            } while (!double.TryParse(donnerArgentTemp, out donnerArgent));
+
+                            if (personnes[j].SiAssezDargent(donnerArgent))
+                            {
+                                personnes[j].RetirerArgent(donnerArgent);
+                                personnes[j - 1].RecevoirArgent(donnerArgent);
+                                Console.WriteLine("Ajout effectué!");
+                            }
+                            else
+                            {
+                                Console.WriteLine(personnes[j].Name + " vous n'avez pas assez d'Argent pour cette transaction");
+                            }
+
+                            break;
+                    }
+                    Console.WriteLine(personnes[0].Name + " a " + personnes[0].PorteMonnaie + " Euros dans son porte monnaie");
+                    Console.WriteLine(personnes[1].Name + " a " + personnes[1].PorteMonnaie + " Euros dans son porte monnaie");
+                }
+
+                Console.WriteLine("Tapez sur espace pour recommencer avec deux autres personnes");
+                recommencer = Console.ReadLine();
+            } while (recommencer == " ");
+            
+            Console.ReadLine();
+        }
+    }
+}
