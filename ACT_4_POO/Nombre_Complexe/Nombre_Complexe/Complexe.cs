55ad1a6140af19ffc25e5990fdb75449fa040ac7
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2022-11-22 14:14:47 +0100

    Nombres Complexes

---
+++ ACT_4_POO/Nombre_Complexe/Nombre_Complexe/Complexe.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nombre_Complexe
+{
+    class Complexe
+    {
+        private double _a;
+        private double _b;
+
+        public Complexe(double a, double b)
+        {
+            this._a = a;
+            this._b = b;
+        }
+
+        public double A
+        {
+            get { return _a; }
+            set { _a = value; }
+        }
+
+        public double B
+        {
+            get { return _b; }
+            set { _b = value; }
+        }
+
+        public void AfficheComplexe(double module, int numeroCom)
+        {
+            string message = "";
+
+            switch (numeroCom)
+            {
+                case 1:
+                    message = "Le premier complexe : (";
+                    break;
+
+                case 2:
+                    message = "Le deuxième complexe : (";
+                    break;
+            }
+
+            message = message + _a.ToString("f2") + "," + _b.ToString("f2") + ") a pour module : " + module.ToString("f2");
+
+            Console.WriteLine(message);
+
+        }
+
+        public double CalculeModule()
+        {
+            double module = Math.Sqrt(Math.Pow(_a, 2) + Math.Pow(_b, 2));
+
+            return module;
+        }
+
+        public void Ajoute(Complexe nbrCom2)
+        {
+            _a += nbrCom2._a;
+            _b += nbrCom2._b;
+
+            Console.WriteLine("Le premier complexe devient : (" + _a.ToString("f2") + "," + _b.ToString("f2") + ") après l'ajout du second:");
+        }
+    }
+}
---
+++ ACT_4_POO/Nombre_Complexe/Nombre_Complexe/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Nombre_Complexe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double aInput;
+            string tryA;
+
+            double bInput;
+            string tryB;
+
+            int numeroCom;
+
+            string refaire = "";
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenue dans ce programme sur les complexes !");
+
+                Console.WriteLine("Que vaut la partie réelle du complexe de départ?");
+                do
+                {
+                    tryA = Console.ReadLine();
+                } while (!double.TryParse(tryA, out aInput));
+
+                Console.WriteLine("Que vaut la partie imaginaire du complexe de départ?");
+                do
+                {
+                    tryB = Console.ReadLine();
+                } while (!double.TryParse(tryB, out bInput));
+
+                Complexe nbrCom1 = new Complexe(aInput, bInput);
+
+                numeroCom = 1;
+                double module = nbrCom1.CalculeModule();
+                nbrCom1.AfficheComplexe(module, numeroCom);
+
+                Console.WriteLine("Encodez un second nombre complexe");
+                Console.ReadLine();
+
+                Console.WriteLine("Que vaut la partie réelle du second complexe?");
+                do
+                {
+                    tryA = Console.ReadLine();
+                } while (!double.TryParse(tryA, out aInput));
+
+                Console.WriteLine("Que vaut la partie imaginaire du second complexe?");
+                do
+                {
+                    tryB = Console.ReadLine();
+                } while (!double.TryParse(tryB, out bInput));
+
+                Complexe nbrCom2 = new Complexe(aInput, bInput);
+
+                numeroCom = 2;
+                module = nbrCom2.CalculeModule();
+                nbrCom2.AfficheComplexe(module, numeroCom);
+
+                nbrCom1.Ajoute(nbrCom2);
+
+                Console.WriteLine("Un autre test ? (Tapez sur espace)");
+                refaire = Console.ReadLine();
+            } while (refaire == " ");
+           
+            Console.ReadLine();
+        }
+    }
+}
